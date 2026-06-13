3bf7f2c1fc62c81d741a36a9135dd40d2a425a0d
Valentin Nastasi <pragmatiku@protonmail.com>
2026-06-13 16:17:55 +0200

    Configure Gradle Develocity and build scans

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,4 +1,3 @@
-rootProject.name = "AtlasMemories"
 enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
 
 pluginManagement {
@@ -30,7 +29,32 @@ dependencyResolutionManagement {
 
 plugins {
     id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
+    id("com.gradle.develocity") version ("4.2.2")
 }
+
+develocity {
+    projectId.set("vnastasi/atlas-memories")
+
+    buildScan {
+        termsOfUseUrl.set("https://gradle.com/help/legal-terms-of-use")
+        termsOfUseAgree.set("yes")
+
+        link("VCS", "https://github.com/vnastasi/atlas-memories")
+
+        if (System.getenv("CI")?.toBooleanStrictOrNull() == true) {
+            tag("GitHub")
+            value("Workflow", System.getenv("GITHUB_WORKFLOW"))
+            value("Run ID", System.getenv("GITHUB_RUN_ID"))
+            value("Run number", System.getenv("GITHUB_RUN_NUMBER"))
+            value("Branch", System.getenv("GITHUB_HEAD_REF")?.takeUnless { it.isBlank() } ?: System.getenv("GITHUB_REF_NAME"))
+            value("Commit ID", System.getenv("GITHUB_SHA"))
+        } else {
+            tag("Local")
+        }
+    }
+}
+
+rootProject.name = "AtlasMemories"
 
 include(":androidApp")
 include(":desktopApp")
